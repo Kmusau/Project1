febb6c306fdb516b8bc3bed8762ce0e79232c290
Sylvester <sylvester.musyoki@cellulant.com>
2019-11-03 16:29:54 +0000

    Move Students.java to the main project package Move /students controller funtion to the main cotroller. Delete controller outside project package

--- src/main/java/com/exampleone/springboot/assignmentone/controlers/AssignmentOneController.java
+++ src/main/java/com/exampleone/springboot/assignmentone/controlers/AssignmentOneController.java
@@ -1,7 +1,11 @@
 package com.exampleone.springboot.assignmentone.controlers;
 
+import com.exampleone.springboot.assignmentone.controlers.DTO.Students;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.Arrays;
+import java.util.List;
 
 /**
  * Created by sylvester
@@ -10,10 +14,20 @@ import org.springframework.web.bind.annotation.RestController;
  * Date: 10/28/19
  * Time: 10:54 AM
  */
-@RestController //This annotation helps to creat a restful controller
+@RestController
 public class AssignmentOneController {
-    @GetMapping("v1/test") // This exposes an endpoint "/v1/test"
-    public String testFunction(){ // This is the function called when the endpoint is invoke. It has a return type string. That means it can only return a string on invokation
-        return "Hello World"; // This is what is being returned on invoking the method. You can return any string you want
+    @GetMapping("v1/test")
+    public String testFunction(){
+        return "Hello World";
+    }
+
+    @GetMapping("/students")
+    public List<Students> studentslist(){
+        return Arrays.asList(
+                new Students("Jesse", "Kiragu"),
+                new Students("Brandon", "Williams"),
+                new Students("Joe", "Henderson")
+        );
+
     }
 }
