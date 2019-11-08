3283c1a5d0d8bb7c83d674643e816c0e905682ab
Kmusau <51605137+Kmusau@users.noreply.github.com>
2019-11-08 22:56:17 +0300

    after adding PUT and POST methods

--- AssignmentOneController.java
+++ AssignmentOneController.java
@@ -5,6 +5,9 @@ import com.exampleone.springboot.assignmentone.controlers.DTO.Students;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.util.List;
@@ -24,8 +27,20 @@ public String testFunction(){
 public List<Students> allstudents(){
     return stservice.studentlist();
 	}
-@GetMapping("/students/{idnum}")
+
+@GetMapping("/student/{idnum}")
 public Students singlestudent(@PathVariable String idnum){
 	return stservice.singlestudent(idnum);
 	}
+
+@PostMapping("/student/create")
+public void addstudent(@RequestBody Students stu){
+	stservice.addstudent(stu);
+	}
+
+@PutMapping("/student/{idnum}")
+public void updatestudent(@RequestBody Students stu, @PathVariable String idnum){
+	stservice.updatestudent(idnum, stu);
+}
+
 }
--- Studentservice.java
+++ Studentservice.java
@@ -1,5 +1,6 @@
 package com.exampleone.springboot.assignmentone.controlers;
 
+import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
 
@@ -9,14 +10,16 @@ import com.exampleone.springboot.assignmentone.controlers.DTO.Students;
 
 @Service
 public class Studentservice {
-	private List<Students>students = Arrays.asList(
+	private List<Students>students = new ArrayList<> (Arrays.asList(
             new Students("Jesse", "Kiragu", "2002100", "Data Science"),
             new Students("Brandon", "Williams", "2002102", "Software Engineering"),
             new Students("Joe", "Henderson", "2002103", "Artificial Intelligence")
-    );
+    ));
 	public List<Students> studentlist(){
 		return students;
 }
+	
+	
 	public Students singlestudent(String idnum){
 		for(Students stude: students){
 			if(stude.getIdnum().equals(idnum)){
@@ -25,4 +28,21 @@ public class Studentservice {
 		}
 		return null;
 	}
+	
+	
+	public void addstudent(Students stu) {
+		students.add(stu);	
+	}
+
+
+	public void updatestudent(String idnum, Students stu) {
+		for(int i =0; i<students.size(); i++){
+			Students stude = students.get(i);
+			if(stude.getIdnum().equals(idnum)){
+				students.set(i, stu);
+					return;
+			}
+		}
+	}
+	
 }
